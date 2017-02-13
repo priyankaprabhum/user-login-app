7c72f8be715f3bb0138b07fe119ff68fa217b834
Rhishi Nema <rhishi.nema@ge.com>
2017-02-12 19:55:39 -0500

    login integration

--- src/main/java/com/user/controller/LoginController.java
+++ src/main/java/com/user/controller/LoginController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.user.model.User;
 import com.user.security.CookieUtil;
 import com.user.security.JwtUtil;
+import com.user.services.LoginService;
 
 import net.sf.json.JSONObject;
 
@@ -24,10 +26,14 @@ import net.sf.json.JSONObject;
 public class LoginController {
 	private static final String jwtTokenCookieName = "JWT-TOKEN";
 	private static final String signingKey = "signingKey";
+
+	@Value("${host}")
+	private String host;
+
 	@Autowired
 	private LoginService loginService;
 
-	@RequestMapping(value = "/login", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@RequestMapping(value = "/json/login", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	public @ResponseBody User login(@RequestBody String userJson) {
 
 		JSONObject jsonObject = JSONObject.fromObject(userJson);
@@ -35,34 +41,39 @@ public class LoginController {
 		user = loginService.checkLoginCredentials(jsonObject.getString("username"), jsonObject.getString("password"));
 		return user;
 	}
+
 	private static final Map<String, String> credentials = new HashMap<>();
-	
-	public LoginController() {
-        credentials.put("hellokoding", "hellokoding");
-        credentials.put("hellosso", "hellosso");
-    }
-	
+
 	@RequestMapping("/")
-    public String home(){
-        return "redirect:/login";
-    }
-	
+	public String home() {
+		return "redirect:/login";
+	}
+
 	@RequestMapping("/login")
-    public String login(){
-        return "login";
-    }
-	
+	public String login() {
+		return "login";
+	}
+
 	@RequestMapping(value = "login", method = RequestMethod.POST)
-    public String login(HttpServletResponse httpServletResponse, String username, String password, String redirect, Model model){
-        if (username == null || !credentials.containsKey(username) || !credentials.get(username).equals(password)){
-            model.addAttribute("error", "Invalid username or password!");
-            return "login";
-        }
+	public String login(HttpServletResponse httpServletResponse, String username, String password, String redirect,
+			Model model) {
+		if (username == null && password == null) {
+			model.addAttribute("error", "Invalid username or password!");
+			return "login";
+		} else {
+			User user = loginService.checkLoginCredentials(username, password);
 
-        String token = JwtUtil.generateToken(signingKey, username);
-        CookieUtil.create(httpServletResponse, jwtTokenCookieName, token, false, -1, "localhost");
+			if (user == null) {
+				model.addAttribute("error", "Invalid username or password!");
+				return "login";
+			}
 
-        return "redirect:" + redirect;
-    }
+			String token = JwtUtil.generateToken(signingKey, username);
+			CookieUtil.create(httpServletResponse, jwtTokenCookieName, token, false, -1, host, user.getFirstName(),
+					user.getLastName());
+
+			return "redirect:" + redirect;
+		}
+	}
 
 }
--- src/main/java/com/user/security/CookieUtil.java
+++ src/main/java/com/user/security/CookieUtil.java
@@ -8,7 +8,7 @@ import org.springframework.web.util.WebUtils;
 
 public class CookieUtil {
 	public static void create(HttpServletResponse httpServletResponse, String name, String value, Boolean secure,
-			Integer maxAge, String domain) {
+			Integer maxAge, String domain, String firstName, String lastName) {
 		Cookie cookie = new Cookie(name, value);
 		cookie.setSecure(secure);
 		cookie.setHttpOnly(true);
@@ -16,6 +16,22 @@ public class CookieUtil {
 		cookie.setDomain(domain);
 		cookie.setPath("/");
 		httpServletResponse.addCookie(cookie);
+
+		Cookie firstNameCookie = new Cookie("first_name", firstName);
+		cookie.setSecure(secure);
+		cookie.setHttpOnly(true);
+		cookie.setMaxAge(maxAge);
+		cookie.setDomain(domain);
+		cookie.setPath("/");
+		httpServletResponse.addCookie(firstNameCookie);
+
+		Cookie lastNameCookie = new Cookie("last_name", lastName);
+		cookie.setSecure(secure);
+		cookie.setHttpOnly(true);
+		cookie.setMaxAge(maxAge);
+		cookie.setDomain(domain);
+		cookie.setPath("/");
+		httpServletResponse.addCookie(lastNameCookie);
 	}
 
 	public static void clear(HttpServletResponse httpServletResponse, String name) {
