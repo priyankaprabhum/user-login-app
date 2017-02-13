7ee4bee54d190e5d98fe08ce7e8383d5d084cf1e
Rhishi Nema <rhishi.nema@ge.com>
2017-02-12 19:23:02 -0500

    added user login HTML

--- src/main/java/com/user/config/UserLoginConfig.java
+++ src/main/java/com/user/config/UserLoginConfig.java
@@ -2,14 +2,21 @@ package com.user.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootConfiguration
-@ComponentScan({ "com.user.*"})
-public class UserLoginConfig {
+@ComponentScan({ "com.user.*" })
+public class UserLoginConfig extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		SpringApplication.run(UserLoginConfig.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(UserLoginConfig.class);
+	}
+
 }
